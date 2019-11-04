ba94d48da1e3b59ef4433858f856ce412854a864
VeenaIyer-17 <iyer.v@northeastern.edu>
2019-11-04 00:03:02 -0500

    Cloud watch agent started

--- inlineprovisioner.sh
+++ inlineprovisioner.sh
@@ -95,6 +95,11 @@ sudo mv cloudwatch-config.json
 cd ~
 sudo wget https://s3.amazonaws.com/amazoncloudwatch-agent/centos/amd64/latest/amazon-cloudwatch-agent.rpm
 sudo rpm -U ./amazon-cloudwatch-agent.rpm
+sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl \
+    -a fetch-config \
+    -m ec2 \
+    -c file:cloudwatch-config.json \
+    -s
 sudo systemctl status amazon-cloudwatch-agent.service
 
 cd ~
